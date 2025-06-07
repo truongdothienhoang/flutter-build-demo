9992d0b2199030f732b7cb1a2d4c2c839d2c4339
Fuchshofer Patrick <33409840+telberiaarbeit@users.noreply.github.com>
2025-06-07 22:21:19 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
-void main() { print('This should now build!'); }
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Demo')),
+        body: const Center(child: Text('Hello from API push')),
+      ),
+    );
+  }
+}
