99b1329f8df4782b8a779a15e4c36153d84c68b2
Fuchshofer Patrick <33409840+telberiaarbeit@users.noreply.github.com>
2025-06-08 09:45:17 +0200

    Update main.dart via API

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(TodoApp());
+void main() => runApp(MyApp());
 
-class TodoApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TodoHome(),
-    );
-  }
-}
-
-class TodoHome extends StatefulWidget {
-  @override
-  _TodoHomeState createState() => _TodoHomeState();
-}
-
-class _TodoHomeState extends State<TodoHome> {
-  final List<String> _tasks = [];
-  final TextEditingController _controller = TextEditingController();
-
-  void _addTask() {
-    if (_controller.text.isNotEmpty) {
-      setState(() {
-        _tasks.add(_controller.text);
-        _controller.clear();
-      });
-    }
-  }
-
-  void _removeTask(int index) {
-    setState(() {
-      _tasks.removeAt(index);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('ToDo App')),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(hintText: 'Enter task'),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: _addTask,
-                ),
-              ],
-            ),
+      title: 'Hello World App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World App'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _tasks.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_tasks[index]),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () => _removeTask(index),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
