cf3c9ba2892aa0a2fc67a88c8937cc432078bfd0
Fuchshofer Patrick <33409840+telberiaarbeit@users.noreply.github.com>
2025-06-08 11:17:03 +0200

    Update main.dart via API

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World App'),
-        ),
-        body: Center(
-          child: Text(
-            'Patrick is great',
-            style: TextStyle(fontSize: 24),
-          ),
+      title: 'Hello World App',
+      home: HelloWorldScreen(),
+    );
+  }
+}
+
+class HelloWorldScreen extends StatefulWidget {
+  @override
+  _HelloWorldScreenState createState() => _HelloWorldScreenState();
+}
+
+class _HelloWorldScreenState extends State<HelloWorldScreen> {
+  String _message = 'Hello World';
+
+  void _updateMessage() {
+    setState(() {
+      _message = 'Today is a great day';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _message,
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _updateMessage,
+              child: Text('Click Me'),
+            ),
+          ],
         ),
       ),
     );
