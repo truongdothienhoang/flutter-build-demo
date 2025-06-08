32627e5c5d222e9ae29b4b2aa21a627e4a7ceb74
Fuchshofer Patrick <33409840+telberiaarbeit@users.noreply.github.com>
2025-06-08 17:30:16 +0200

    Update push.js

--- api/push.js
+++ api/push.js
@@ -4,7 +4,7 @@ const fetch = global.fetch || ((...args) =>
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN; // Or hardcode for now
 const REPO_OWNER = 'telberiaarbeit';
 const REPO_NAME = 'flutter-build-demo';
-const BRANCH = 'main';
+const BRANCH = 'web-build';
 const FILE_PATH = 'lib/main.dart';
 
 export default async function handler(req, res) {
